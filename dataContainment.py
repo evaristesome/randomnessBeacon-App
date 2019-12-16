06c8f95fe2f710904204f2eb998c1953cd02e4cf
evso9816 <evso9816@colorado.edu>
2019-12-16 12:26:08 -0700

    Update in readme and dataContainment

--- dataContainment.py
+++ dataContainment.py
@@ -1,112 +1,162 @@
 
 import sqlite3
-from sqlite3 import Error
 from convertion import parseHourDayMonthYear
 con = sqlite3.connect('randomnessBeacon.db')
 
-hourBeacon =0
-dayBeacon = 0
-monthBeacon = 0
-yearBeacon = 0
+import logging
+logging.basicConfig(filename='RNGLog.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+#logger=logging.getLogger(__name__)
 
 class data:
 
-    # Create table
+    #sqlTable creates table structure if it does not exist
     def sqlTable(self):
-        objectCursor = con.cursor()
-        # need to rethink on the table structure: pulseDay integer, pulseMonth integer,pulseYear integer
-        objectCursor.execute("""CREATE TABLE IF NOT EXISTS beaconsDB 
-                            (ID integer PRIMARY KEY AUTOINCREMENT, 
-                            currentTimeCur text, 
-                            hashPrev text, 
-                            beaconH text,
-                            beaconD text,
-                            beaconM text,
-                            beaconY text,
-                            hashCur text,  
-                            precommitment text)""")
-        con.commit()
+        try:
+            objectCursor = con.cursor()
+            # need to rethink  the table structure: pulseDay integer, pulseMonth integer,pulseYear integer
+            # to be inserted later: sourceRNG1 text, sourceRNG2 text, sourceHSM text, sourcePaul text
+            objectCursor.execute("""CREATE TABLE IF NOT EXISTS beaconsDB 
+                                (ID integer PRIMARY KEY AUTOINCREMENT, 
+                                currentTimeCur text,        
+                                outputValuePrev text, 
+                                beaconH text,
+                                beaconD text,
+                                beaconM text,
+                                beaconY text,
+                                outputValueCur text,  
+                                hashNext text)""")
+            con.commit()
+        except Exception as e:
+            logging.error("Exception occured: ", exc_info=True)
 
-    # insert data into the database
+    ''' 
+    inserteaconsinfo function inserts data into the database
+    '''
     def insertBeaconsinfo(self,entities):
-        objectCursor = con.cursor()
-        objectCursor.execute(
-            '''INSERT INTO beaconsDB(currentTimeCur, hashPrev, beaconH, beaconD, beaconM, beaconY, hashCur, precommitment) VALUES(?,?,?,?,?,?,?,?)''', entities)
-        # entities contain the different field to be inserted
-        con.commit()
+        try:
+            objectCursor = con.cursor()
+            objectCursor.execute(
+                '''INSERT INTO beaconsDB(currentTimeCur, outputValuePrev, beaconH, beaconD, beaconM, beaconY, outputValueCur, hashNext) VALUES(?,?,?,?,?,?,?,?)''', entities)
+            #to be inserted later: sourceRNG1, sourceRNG2, sourceHSM, sourcePaul
+            # entities contain the different field to be inserted
+            con.commit()
+        except Exception as e:
+            logging.error("Exception occured: ", exc_info=True)
 
+    '''
+    collectData function dequeues data from beaconEngine4 beaconEngineProcess/engine, displays and stores them in the database.
+    The first time data are received, n = 1, they are buffered in containers with the extension Prev, i.e. previous.
+    The second time data are received, n = 2, they are buffered in containers with the extension Cur, i.e. current.
+    The third time data are received, n = 3, they are buffered in containers with the extension Next.
+    After the first three rounds, everything plays out in n = 4:
+        First: we display all buffered Prev, Cur, and Next. Pushing data to database happens in this phase
+        Second: we buffer all Cur variables to Prev, Nexts to Curs, and dequeueing the queue to Nexts
+        Third: we go back to First
+    '''
     def collectData(self, queueS1, queueS2):
-        n=1
-        # constantH, beaconH, constantD, beaconD, constantM, beaconM, constantY, beaconY
-        constantH = 60
-        beaconH = 60
-        constantD = 60
-        beaconD = 60
-        constantM = 60
-        beaconM = 60
-        constantY = 60
-        beaconY = 60
-        while True:
-            while not queueS1.empty():
+        try:
+            n=1     # This constant serves for testing purpose to buffer data in previous, current, and next variables.
+            # These constants serve for testing purpose of the hour, day, month, and year
+            constantH = 60
+            beaconH = 60
+            constantD = 60
+            beaconD = 60
+            constantM = 60
+            beaconM = 60
+            constantY = 60
+            beaconY = 60
+            while True:
+                while not queueS1.empty():
 
-                if n==1:
-                    precommitmentPrev = queueS1.get()
-                    hashPrev = queueS1.get()
-                    currentTimePrev = queueS1.get()
-                    queueS1.empty()
-                    n=n+1
-                if n==2:
-                    precommitmentCur = queueS1.get()
-                    hashCur = queueS1.get()
-                    currentTimeCur = queueS1.get()
-                    queueS1.empty()
-                    n = n + 1
-                if n==3:
-                    precommitment = queueS1.get()
-                    hashNext = queueS1.get()
-                    currentTimeNext = queueS1.get()
-                    queueS1.empty()
-                    n=n+1
-                if n==4:
-                    hourT, dayT, monthT, yearT = parseHourDayMonthYear(currentTimeCur)
+                    ''' 
+                    These tests allow to keep data into variables, determine later what is previous, current or next.
+                    '''
+                    if n==1:
+                        outputValuePrev = queueS1.get()     #precommitmentPrev = queueS1.get()   #
+                        hashPrev = queueS1.get()            #
+                        currentTimePrev = queueS1.get()     #
+                        sourceRNG1 = queueS1.get()          #
+                        sourceRNG2 = queueS1.get()          #
+                        sourceHSM = queueS1.get()           #
+                        sourcePaul = queueS1.get()
+                        queueS1.empty()
+                        n=n+1
+                    if n==2:
+                        outputValueCur = queueS1.get()      #precommitmentCur = queueS1.get()    #
+                        hashCur = queueS1.get()             #
+                        currentTimeCur = queueS1.get()      #
+                        sourceRNG1 = queueS1.get()          #
+                        sourceRNG2 = queueS1.get()          #
+                        sourceHSM = queueS1.get()           #
+                        sourcePaul = queueS1.get()
+                        queueS1.empty()
+                        n = n + 1
+                    if n==3:
+                        outputValueNext = queueS1.get()     #precommitment = queueS1.get()       #
+                        hashNext = queueS1.get()            #
+                        currentTimeNext = queueS1.get()     #
+                        sourceRNG1Next = queueS1.get()          #
+                        sourceRNG2Next = queueS1.get()          #
+                        sourceHSMNext = queueS1.get()           #
+                        sourcePaulNext = queueS1.get()
+                        queueS1.empty()
+                        n=n+1
+                    if n==4:
+                        hourT, dayT, monthT, yearT = parseHourDayMonthYear(currentTimeCur)
 
-                    if hourT != constantH:
-                        beaconH = hashCur
-                        constantH =hourT
+                        if hourT != constantH:
+                            beaconH = outputValueCur       #     hashCur
+                            constantH = hourT
 
-                    if dayT != constantD:
-                        beaconD = hashCur
-                        constantD = dayT
+                        if dayT != constantD:
+                            beaconD = outputValueCur       #     hashCur
+                            constantD = dayT
 
-                    if monthT != constantM:
-                        beaconM = hashCur
-                        constantH =hourT
+                        if monthT != constantM:
+                            beaconM = outputValueCur       #     hashCur
+                            constantM = monthT
 
-                    if yearT != constantY:
-                        beaconY = hashCur
-                        constantY = yearT
+                        if yearT != constantY:
+                            beaconY = outputValueCur       #outputValueCur     hashCur
+                            constantY = yearT
 
-                    print('Time: ', currentTimeCur)
-                    print('Previous beacon: ', hashPrev)
-                    print('Hour:', beaconH)                 #not yet in DB
-                    print('Day: ', beaconD)                 #not yet in DB
-                    print('Month: ', beaconM)               #not yet in DB
-                    print('Year: ', beaconY)                #not yet in DB
-                    print('Current beacon: ', hashCur)
-                    print('Precommitment: ', precommitment)
-                    print('Signature: ')
-                    print()
+                        print('Time: ', currentTimeCur)
+                        print('Source RNG_1: ', sourceRNG1)         #Additional
+                        print('Source RNG_2:  ', sourceRNG2)        #Additional
+                        print('Source RNG_HSM: ', sourceHSM)        #Additional
+                        print('Source Paul RNG: ', sourcePaul)
+                        print('Previous beacon: ', outputValuePrev)        #    hashPrev
+                        print('Hour:', beaconH)
+                        print('Day: ', beaconD)
+                        print('Month: ', beaconM)
+                        print('Year: ', beaconY)
+                        print('Current beacon: ', outputValueCur)          #     hashCur
+                        print('Precommitment: ', hashNext)     #        precommitment
+                        print('Signature: ')
+                        print()
 
-                    currentTimeCur = currentTimeNext
-                    hashPrev = hashCur
-                    hashCur = hashNext
+                        currentTimeCur = currentTimeNext
+                        outputValuePrev = outputValueCur      #        hashPrev = hashCur
+                        outputValueCur = outputValueNext      #      hashCur = hashNext
+                        sourceRNG1 = sourceRNG1Next
+                        sourceRNG2 = sourceRNG2Next
+                        sourceHSM = sourceHSMNext
+                        sourcePaul = sourcePaulNext
 
-                    precommitment = queueS1.get()
-                    hashNext = queueS1.get()
-                    currentTimeNext = queueS1.get()
+                        outputValueNext = queueS1.get()       #outputValueNext
+                        hashNext = queueS1.get()            #
+                        currentTimeNext = queueS1.get()
+                        sourceRNG1Next = queueS1.get()          #will add it to the dB lter
+                        sourceRNG2Next = queueS1.get()          #will add it to the dB lter
+                        sourceHSMNext = queueS1.get()           #will add it to the dB lter
+                        sourcePaulNext = queueS1.get()           #will add it to the dB lter
 
-                    self.sqlTable()
-                    entities =(str(currentTimeCur), str(hashPrev), str(beaconH), str(beaconD), str(beaconM), str(beaconY), str(hashCur), str(precommitment))
-                    self.insertBeaconsinfo(entities)
+                        self.sqlTable()
+                        entities =(str(currentTimeCur), str(outputValuePrev), str(beaconH), str(beaconD), str(beaconM), str(beaconY), str(outputValueCur), str(hashNext))
+                        #entities = (str(currentTimeCur), str(sourceRNG1), str(sourceRNG2), str(sourceHSM), str(sourcePaul), str(outputValuePrev), str(beaconH), str(beaconD), str(beaconM),str(beaconY), str(outputValueCur), str(hashNext))
+                        self.insertBeaconsinfo(entities)
+        except Exception as e:
+            logging.error("Exception occured: ", exc_info=True)
+
 #if __name__=='__main__':
     #objectContainment = dataContainment()
---
+++ readMe.md
@@ -0,0 +1,49 @@
+﻿Randomness Beacon Generation Program
+
+The program randomnessBeacon consists of generating truly random numbers. 
+It combines different tasks and used several functions to reach its goal. 
+The current version uses Yubico Hardware Security Module 2, but can be easily modified to run without HSM.
+The program uses Python programming and contains essentially six files.
+
+cryptoHeaven:
+It offers the capability to 
+	- Open a session on the HSM, openSession(), and return the session ID;
+	- Authenticate, authenticate(ID), and return the key, and a random number generated from the HSM;
+	- Sign multiple messages, complexSigning(x,y,z,w,k) where “k” is the key;
+	- Sign a single message, SingleSigning(m, k);
+	- Delete the key generated from the HSM, cleanUp(k);
+	- Main function, main(session), retrieves RNG from external sources, authenticate the session, signed combined and individual random numbers. “main(session)” returns its outputs to rngServer assemble_message(session).
+
+rngServer:
+Offers the following tasks to:
+	- Assemble messages, assemble_message(session), received from the HSM;
+	- RNG Handler, rng_handler(x), look for incoming process;
+	- Create socket, create_socket(y), for the RNG server.
+
+beaconEngine4:
+It offers the following tasks:
+	- Getting the random numbers, get_rng(socket),  from the server rngServer, and return it to engine(q1,q2);
+	- Create RNG client socket, create_rng_client_socket(x);
+	- Start the RNG clients, start_rng_clients(x);
+	- Start a single RNG, start_single_rng(x);
+	- Start RNGs in their own process, start_rngs(x);
+	- Stop RNGs, stop_rngs(x,y), to clean up and stop the program;
+	- Engine, engine(q1,q2), to enqueue data received from get_rng(socket)
+
+RNGs:
+Offers the following tasks:
+	- A generic function, RNG(), to generate a random number and return it to the HSM;
+	- Paul RSA simple algorithm, PaulRSASimple(), to generate a random number and return it to the HSM;
+	- Parsing function, parseHourDayMonthYear() ----→ will be removed soon
+
+dataContainment:
+offers the following tasks:
+	- Sql table definition, sqlTable(), to define the dB structure. Current version uses SQLite3;
+	- Retrieve data from the queue, collectData(q1,q2), and process it;
+	- Store beacons, insertBeaconinfo(x) into the database;
+	 
+queueTransit:
+Allows two different processes, beaconEngine4 engine(q1,q2) and dataContainment colectData(q1,q2), to exchange data, i.e. to enqueue and dequeue.
+
+
+
